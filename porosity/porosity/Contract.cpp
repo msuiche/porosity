bdd48992d9ea21235ce8b4046b3cf0cf1b8a70ad
Matt Suiche <m@comae.io>
2017-06-23 15:12:50 +0400

    BUGFIX: SSTORE and constants.

--- porosity/porosity/Contract.cpp
+++ porosity/porosity/Contract.cpp
@@ -973,7 +973,22 @@ Contract::decompileBlock(
             break;
         case Instruction::SSTORE:
         {
-            string valueName = (i->stack[1].exp.size()) ? i->stack[1].exp : i->stack[1].name;
+
+            string valueName = "";
+            switch (i->stack[1].type) {
+                case ConstantComputed:
+                case Constant:
+                {
+                    stringstream mod;
+                    mod << "0x" << std::hex << i->stack[1].value;
+                    valueName = mod.str();
+                    // mod << "0x" << std::hex << second->value;
+                    break;
+                }
+                default:
+                    valueName = (i->stack[1].exp.size()) ? i->stack[1].exp : i->stack[1].name;
+                    break;
+            }
             exp = "store[" + i->stack[0].name + "] = " + valueName + ";";
             if (_block->Flags & BlockFlags::NoMoreSSTORE) errCode |= DCode_Err_ReentrantVulnerablity;
             break;
